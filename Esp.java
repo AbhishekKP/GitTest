47983679b797616c88ed90de24b377947432eb44
RiposteArun <bsarun7@yahoo.in>
2015-01-09 15:20:25 +0530

    adding new class

---
+++ Esp.java
@@ -0,0 +1,20 @@
+public class Person{
+	private String empid;
+	private String name;
+
+	public void setEmpID(String empid){
+		this.empid = empid;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public String getEmpID(){
+		return empid;
+	}
+}
